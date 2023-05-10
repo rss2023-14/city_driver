b238b506d713be65309f6e4e4e26de4e3d1c4f74
joposada7 <joposada7@gmail.com>
2023-05-10 16:46:23 -0400

    add params

--- src/line_controller.py
+++ src/line_controller.py
@@ -16,9 +16,12 @@ class LineFollowingController():
         DRIVE_TOPIC = rospy.get_param("drive_topic")
         self.drive_pub = rospy.Publisher(DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
 
-        self.parking_distance = 0.75  # meters
-        self.relative_x = 0
-        self.relative_y = 0
+        self.SPEED = rospy.get_param("speed")
+        self.MAX_STEERING_ANGLE = np.deg2rad(rospy.get_param("max_steering_angle"))
+        self.STEERING_ANGLE_VELOCITY = np.deg2rad(rospy.get_param("steering_angle_velocity"))
+        self.Kp = rospy.get_param("Kp")
+        self.Ki = rospy.get_param("Ki")
+        self.Kd = rospy.get_param("Kd")
 
         self.prev_time = rospy.Time.now()
 
@@ -33,13 +36,13 @@ class LineFollowingController():
         dt = (time - self.prev_time).to_sec()
         self.prev_time = time
 
-        self.relative_x = msg.x # x in front
-        self.relative_y = msg.y # y to left
+        x = msg.x # x in front
+        y = msg.y # y to left
 
-        dist = np.sqrt((self.relative_x**2.0)+(self.relative_y**2.0))
+        dist = np.sqrt((x**2.0)+(y**2.0))
         dist_err = dist
 
-        theta_err = np.arctan2(self.relative_y, self.relative_x) # theta to left
+        theta_err = np.arctan2(y, x) # theta to left
 
         self.running_theta_err += theta_err
         self.running_dist_err += dist_err
@@ -54,12 +57,15 @@ class LineFollowingController():
         drive_cmd.header.stamp = rospy.Time.now()
         drive_cmd.header.frame_id = "base_link"
 
-        drive_cmd.drive.steering_angle = 0.4 * theta_err + \
-            0.01 * d_theta_dt + 0.0 * self.running_theta_err
+        # Find angle and cap it
+        angle = self.Kp * theta_err + \
+            self.Kd * d_theta_dt + self.Ki * self.running_theta_err
+        angle = np.sign(angle)*max(abs(angle), self.MAX_STEERING_ANGLE)
+        drive_cmd.drive.steering_angle = angle
 
-        drive_cmd.drive.speed = 1.0
+        drive_cmd.drive.speed = self.SPEED
+        drive_cmd.drive.steering_angle_velocity = self.STEERING_ANGLE_VELOCITY
 
-        drive_cmd.drive.steering_angle_velocity = 0.0
         drive_cmd.drive.acceleration = 0.0
         drive_cmd.drive.jerk = 0.0
 
